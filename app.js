7b7209bdf68d3fd6f49c3bc54307c715b835b3c8
aramik <aramik@whalerockindustries.com>
2014-07-31 10:18:47 -0700

    updated force domain to correct domain in app.js

--- app.js
+++ app.js
@@ -16,6 +16,8 @@ app.engine('html',swig.renderFile);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'html');
 
+app.use( require('express-force-domain')('http://www.blissautoleasing.com') );
+
 app.use(favicon());
 app.use(logger('dev'));
 app.use(bodyParser.json());
