3120950d29ec91dc3ef7a62270dd150fb2d3fc4b
aramik <aramik@whalerockindustries.com>
2014-08-01 00:46:35 -0700

    test

--- routes/index.js
+++ routes/index.js
@@ -3,9 +3,9 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-	var dbURL = "speacials";
+	var dbURL = "specials";
 	var collections = "cars";
-	var db = require("mongojs").connect(dbURL,collections);
+	var db = require("mongojs").connect(dbURL);
 
   res.render('index', db.specials.find());
 });
