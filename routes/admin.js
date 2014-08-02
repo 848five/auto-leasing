e90a0c5fb75635c2aed58a925c8de76587df400e
aramik <aramik@whalerockindustries.com>
2014-08-02 02:03:38 -0700

    Added session

--- routes/admin.js
+++ routes/admin.js
@@ -3,12 +3,14 @@ var md5 = require('MD5');
 var router = express.Router();
 var user;
 var mongodb = require('mongodb');
-var db = new mongodb.Db('bliss', new mongodb.Server('127.0.0.1', 27017), {safe:true});
+
 router.get('/', function(req,res) {
          res.render('admin',"");
 });
 
 function loadSpecials() {
+	var db = new mongodb.Db('bliss', new mongodb.Server('127.0.0.1', 27017), {safe:true});
+	db.open(function(err) {
 			db.collection('specials',function(err,collection) {
 				var test = collection.find({}).toArray(function(err,list) {
 							console.log(list);
@@ -18,6 +20,7 @@ function loadSpecials() {
 				console.log('test' + test);
 				
 			});
+		}
 }
 
 
@@ -25,6 +28,7 @@ router.post('/create/:collection?',function(req,res) {
 	switch(req.params.collection) {
     case 'specials':
     	var year = req.body.year;
+    	var db = new mongodb.Db('bliss', new mongodb.Server('127.0.0.1', 27017), {safe:true});
         db.open(function(err) {
         	if (!err) {
 				db.collection('specials',function(err,collection) {
