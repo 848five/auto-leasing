d979c04fa16ac30b91e3aefcc4b33ee451ace534
aramik <aramik@whalerockindustries.com>
2014-08-01 20:59:19 -0700

    testing

--- routes/admin.js
+++ routes/admin.js
@@ -4,13 +4,13 @@ var router = express.Router();
 var mongodb = require('mongodb');
 var db = new mongodb.Db('bliss', new mongodb.Server('127.0.0.1', 27017), {safe:true});
 router.get('/', function(req,res) {
-         res.render('admin',{items: items});
+         res.render('admin',"");
 });
 
 
 router.post('/', function(req,res) {
 	console.log(req.body.user);
-	return;
+
 db.open(function(err) {
 	db.collection('admin',function(err,collection) {
 		collection.find().toArray(function(err,items) {
