a3855cc07c598caa9632e9e20d822527276daa27
aramik <aramik@whalerockindustries.com>
2014-08-02 01:09:16 -0700

    Added session

--- routes/admin.js
+++ routes/admin.js
@@ -18,6 +18,7 @@ router.post('/create/:collection?',function(req,res) {
 				db.collection('specials',function(err,collection) {
 					  collection.save({year:year} , function(err, result) {
          console.log('special saved');
+         console.log(user);
 					res.render('admin',{auth: user,msg: 'record added.'});
       });
 					
