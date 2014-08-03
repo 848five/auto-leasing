3bced558cd77a266d08770cbecd982273d762959
aramik <aramik@whalerockindustries.com>
2014-08-02 19:45:31 -0700

    cleaned app.js

--- routes/index.js
+++ routes/index.js
@@ -7,7 +7,6 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res) {
 		res.render('index',"");
-		console.log(req.session.name);
 });
 
 module.exports = router;
