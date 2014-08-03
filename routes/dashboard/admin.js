e0cabb64ded29c8d948ca61bd8dcb6831e047253
aramik <aramik@whalerockindustries.com>
2014-08-02 22:54:25 -0700

    up

--- routes/dashboard/admin.js
+++ routes/dashboard/admin.js
@@ -5,6 +5,20 @@ var user;
 var specialsList;
 var mongodb = require('mongodb');
 
+
+router.get('/dashboard/:category?/:year?/:make?/:model?', function(req,res) {
+	if (model) {
+		res.send('model level');
+	} else if (make) {
+		res.send('make level');
+	} else if (year) {
+		res.send('year level');
+	} else if (category) {
+		res.send('category');
+	}
+});
+
+
 router.get('/', function(req,res) {
 		 var hash = new Date();
 		 var activeSession = req.cookies._a;
