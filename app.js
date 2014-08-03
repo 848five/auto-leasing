0d0c819c1a6aad69c1f17ace01009e14c97d39bf
aramik <aramik@whalerockindustries.com>
2014-08-03 00:49:36 -0700

    up

--- app.js
+++ app.js
@@ -134,7 +134,7 @@ app.get('/dashboard/:category?/:year?/:make?/:model?', function(req,res) {
                 });
             } 
         } else {
-            res.send('not authorized');
+            res.render('admin',"");
         }
 });
 
