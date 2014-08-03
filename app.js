0176126e4eb4a6d7bc67858447b3254df0406c18
aramik <aramik@whalerockindustries.com>
2014-08-03 00:27:47 -0700

    up

--- app.js
+++ app.js
@@ -38,48 +38,64 @@ app.use(express.static(path.join(__dirname, 'static')));
 
 
 //Routes
-app.get('/dashboard/:category?/:year?/:make?/:model?', function(req,res) {
+app.get('/dashboard/:category?/:year?/:make?/:modle?', function(req,res) {
     var category = req.params.category;
     var year = req.params.year;
     var make = req.params.make;
-    var model = req.params.model;
+    var modle = req.params.modle;
 
-    if (model) {
+    if (modle) {
         var db = new mongodb.Db('bliss', new mongodb.Server('127.0.0.1', 27017), {safe:true});
-         db.open(function(err) {
+        db.open(function(err) {
             db.collection(category,function(err,collection) {
-                    if (year == 'all') {
+                   if (year == "all") {
                         collection.find({make:make,model:model},{sort:{$natural:-1}}).toArray(function(err,list) {
+                        categoryList = list;
+                        if (categoryList.length == 0 || category == "admin") {
+                            res.send('nothing found');
+                        } else {
+                            res.send(categoryList);
+                        }
+                        db.close();
+                        });             
                     } else {
                         collection.find({year:year,make:make,model:model},{sort:{$natural:-1}}).toArray(function(err,list) {
+                        categoryList = list;
+                        if (categoryList.length == 0 || category == "admin") {
+                            res.send('nothing found');
+                        } else {
+                            res.send(categoryList);
+                        }
+                        db.close();
+                        }); 
                     }
-                    categoryList = list;
-                    if (categoryList.length == 0 || category == "admin") {
-                        res.send('nothing found');
-                    } else {
-                        res.send(categoryList);
-                    }
-                    db.close();
-                });             
             });
         });
     } else if (make) {
-         var db = new mongodb.Db('bliss', new mongodb.Server('127.0.0.1', 27017), {safe:true});
-         db.open(function(err) {
+        var db = new mongodb.Db('bliss', new mongodb.Server('127.0.0.1', 27017), {safe:true});
+        db.open(function(err) {
             db.collection(category,function(err,collection) {
-                    if (year == "all") {
+                   if (year == "all") {
                         collection.find({make:make},{sort:{$natural:-1}}).toArray(function(err,list) {
+                        categoryList = list;
+                        if (categoryList.length == 0 || category == "admin") {
+                            res.send('nothing found');
+                        } else {
+                            res.send(categoryList);
+                        }
+                        db.close();
+                        });             
                     } else {
                         collection.find({year:year,make:make},{sort:{$natural:-1}}).toArray(function(err,list) {
+                        categoryList = list;
+                        if (categoryList.length == 0 || category == "admin") {
+                            res.send('nothing found');
+                        } else {
+                            res.send(categoryList);
+                        }
+                        db.close();
+                        }); 
                     }
-                    categoryList = list;
-                    if (categoryList.length == 0 || category == "admin") {
-                        res.send('nothing found');
-                    } else {
-                        res.send(categoryList);
-                    }
-                    db.close();
-                });             
             });
         });
     } else if (year) {
