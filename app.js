cbdd10a935045c71e0cbbe00df5830a787c60a46
aramik <aramik@whalerockindustries.com>
2014-08-04 00:44:23 -0700

    test

--- app.js
+++ app.js
@@ -30,7 +30,7 @@ app.use(express.static(path.join(__dirname, 'static')));
 
 app.use(multer({
  
-          dest: '/root/auto-leasing/static/uploads';
+          dest: "/root/auto-leasing/static/uploads"
  
 }));
 
