3865f20d821b056bdce5bfb040fc57c97d8a1376
aramik <aramik@whalerockindustries.com>
2014-08-03 20:32:14 -0700

    test

--- routes/dashboard/admin.js
+++ routes/dashboard/admin.js
@@ -14,6 +14,7 @@ router.get('/', function(req,res) {
 		 	console.log(activeSession);
 		 	res.render('tools',"");
 		 } else {
+		 	console.log(activeSession);
          	res.render('admin',"");
 		 }
 });
