550985ad950ce0c6559b5890cafc5162ddd0caab
aramik <aramik@whalerockindustries.com>
2014-08-04 02:47:18 -0700

    test

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,113 @@
 var express = require('express');
-
+var mongodb = require('mongodb');
 var router = express.Router();
 
 
 
 /* GET home page. */
+router.get('/:category?/:year?/:make?/:model?', function(req,res,next) {
+
+    var category = req.params.category;
+    var year = req.params.year;
+    var make = req.params.make;
+    var model = req.params.model;
+ 
+            if (model) {
+                db.open(function(err) {
+                    db.collection(category,function(err,collection) {
+                           if (year == "all") {
+                                collection.find({make:make,model:model},{sort:{$natural:-1}}).toArray(function(err,list) {
+                                categoryList = list;
+                                if (categoryList.length == 0 || category == "admin") {
+                                    res.send('nothing found');
+                                } else {
+                                    res.render('specials',{list:categoryList});
+                                }
+                                db.close();
+                                });             
+                            } else {
+                                collection.find({year:year,make:make,model:model},{sort:{$natural:-1}}).toArray(function(err,list) {
+                                categoryList = list;
+                                if (categoryList.length == 0 || category == "admin") {
+                                    res.send('nothing found');
+                                } else {
+                                    res.render('specials',{list:categoryList});
+                                }
+                                db.close();
+                                }); 
+                            }
+                    });
+                });
+            } else if (make) {
+                db.open(function(err) {
+                    db.collection(category,function(err,collection) {
+                           if (year == "all") {
+                                collection.find({make:make},{sort:{$natural:-1}}).toArray(function(err,list) {
+                                categoryList = list;
+                                if (categoryList.length == 0 || category == "admin") {
+                                    res.send('nothing found');
+                                } else {
+                                    res.render('specials',{list:categoryList});
+                                }
+                                db.close();
+                                });             
+                            } else {
+                                collection.find({year:year,make:make},{sort:{$natural:-1}}).toArray(function(err,list) {
+                                categoryList = list;
+                                if (categoryList.length == 0 || category == "admin") {
+                                    res.send('nothing found');
+                                } else {
+                                    res.render('specials',{list:categoryList});
+                                }
+                                db.close();
+                                });  
+                            }
+                    });
+                });
+            } else if (year) {
+                db.open(function(err) {
+                    db.collection(category,function(err,collection) {
+	                    if (year == "all") {
+	                    		collection.find({},{sort:{$natural:-1}}).toArray(function(err,list) {
+	                            categoryList = list;
+	                            if (categoryList.length == 0 || category == "admin") {
+	                                res.send('nothing found');
+	                            } else {
+	                                res.render('specials',{list:categoryList});
+	                            }
+	                            db.close();
+	                        	});
+	                    } else {
+	                           collection.find({year:year},{sort:{$natural:-1}}).toArray(function(err,list) {
+	                            categoryList = list;
+	                            if (categoryList.length == 0 || category == "admin") {
+	                                res.send('nothing found');
+	                            } else {
+	                                res.render('specials',{list:categoryList});
+	                            }
+	                            db.close();
+	                        	});             
+                		}
+                    });
+                });
+            } else if (category) {
+                db.open(function(err) {
+                    db.collection(category,function(err,collection) {
+                           collection.find({},{sort:{$natural:-1}}).toArray(function(err,list) {
+                            categoryList = list;
+                            if (categoryList.length == 0 || category == "admin") {
+                                res.send('nothing found');
+                            } else {
+                                res.render('specials',{list:categoryList});
+                            }
+                            db.close();
+                        });             
+                    });
+                });
+            } 
+        
+});
+
 router.get('/', function(req, res) {
 		res.render('index',"");
 });
