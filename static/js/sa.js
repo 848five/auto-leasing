0f08867ac7cbd28dfa3b157add0fb1c78a908b47
aramik <aramik@whalerockindustries.com>
2014-08-05 15:34:24 -0700

    small

--- static/js/sa.js
+++ static/js/sa.js
@@ -5,8 +5,21 @@ window.onload = function() {
 			alert($(this).attr('data-item'));
 		});
 
+		$('.js-remove-photo').click(function(e) {
+			e.preventDefault();
+			if (confirm('Are you sure you want to remove this photo?')) {
+			    removePhoto();
+			} else {
+			}
+		});
+
 		$('.js-edit-btn').click(function(e) {
 			e.preventDefault();
 			alert($(this).attr('data-item'));
 		});
+}
+
+
+function removePhoto(id,photo) {
+	alert('removed');
 }
