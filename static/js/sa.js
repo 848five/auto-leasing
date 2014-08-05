caed05a18fb376c63045ae8ff59d6091891ab415
aramik <aramik@whalerockindustries.com>
2014-08-05 15:05:43 -0700

    small

--- static/js/sa.js
+++ static/js/sa.js
@@ -1,12 +1,12 @@
 window.onload = function() {
 
-		$('.js-add-photo-btn').addEventListener('click',function(e) {
+		$('.js-add-photo-btn').click(function(e) {
 			e.preventDefault();
 			alert($(this).attr('data-item'));
-		},false);
+		});
 
-		$('.js-edit-btn').addEventListener('click',function(e) {
+		$('.js-edit-btn').click(function(e) {
 			e.preventDefault();
 			alert($(this).attr('data-item'));
-		},false);
+		});
 }
