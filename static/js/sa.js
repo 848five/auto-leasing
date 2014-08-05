74fccc0905df3f372d1673d9ef3c49ec2fb9943a
aramik <aramik@whalerockindustries.com>
2014-08-05 14:25:56 -0700

    small

--- static/js/sa.js
+++ static/js/sa.js
@@ -3,14 +3,14 @@ window.onload = function() {
 	if (addPhotoBtn = document.querySelector('.js-add-photo-btn')) {
 		$(addPhotoBtn).addEventListener('click',function(e) {
 			e.preventDefault();
-			
+			alert('this.attr('data-item')');
 		},false);
 	}
 
 	if (editBtn = document.querySelector('.js-edit-btn')) {
 		$(editBtn).addEventListener('click',function(e) {
 			e.preventDefault();
-
+			alert('this.attr('data-item')');
 		},false);
 	}
 }
