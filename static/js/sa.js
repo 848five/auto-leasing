531db733f37254f4f756d68b876eac0a28ffde81
aramik <aramik@whalerockindustries.com>
2014-08-05 14:56:02 -0700

    small

--- static/js/sa.js
+++ static/js/sa.js
@@ -1,13 +1,15 @@
 window.onload = function() {
+	var addPhotoBtn = document.querySelector('.js-add-photo-btn');
+	var editBtn = document.querySelector('.js-edit-btn');
 
-	if (addPhotoBtn = document.querySelector('.js-add-photo-btn')) {
+	if (addPhotoBtn) {
 		$(addPhotoBtn).addEventListener('click',function(e) {
 			e.preventDefault();
 			alert($(this).attr('data-item'));
 		},false);
 	}
 
-	if (editBtn = document.querySelector('.js-edit-btn')) {
+	if (editBtn) {
 		$(editBtn).addEventListener('click',function(e) {
 			e.preventDefault();
 			alert($(this).attr('data-item'));
