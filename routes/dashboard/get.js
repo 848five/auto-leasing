9b086798f9883052e5237130301c3ad430039010
aramik <aramik@whalerockindustries.com>
2014-08-05 17:30:21 -0700

    small

--- routes/dashboard/get.js
+++ routes/dashboard/get.js
@@ -29,7 +29,7 @@ router.post('/:category',function(req,res,next) {
 
             for (image in req.files) {
                     console.log('=================================>' + image);
-                for (path in image) {
+                for (path in image.path) {
                     console.log('=================================>' + path);
                 }
             }
