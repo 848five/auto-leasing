6cf404a71a412147ef4cb4324c95aadd5b345726
aramik <aramik@whalerockindustries.com>
2014-08-07 01:29:24 -0700

    milesonte api complete

--- static/js/sa.js
+++ static/js/sa.js
@@ -26,3 +26,15 @@ function removePhoto(id,photo) {
 	console.log("id:->" + id);
 	console.log("photo:->" + photo);
 }
+
+
+/*
+$.ajax({
+    url: 'http://www.blissautoleasing.com/dashboard/view/photo/specials/53e1ad2edfbea44b651fdb4c',
+    type: 'DELETE',
+    data: {'photo':'test.jpg'},
+    success: function(result) {
+        console.log(result);
+    }
+});
+*/
