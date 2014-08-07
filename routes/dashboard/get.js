cd8c4b8b8321356c8d50d546f02c75cd6a28f0a0
aramik <aramik@whalerockindustries.com>
2014-08-07 00:04:45 -0700

    added delete rout

--- routes/dashboard/get.js
+++ routes/dashboard/get.js
@@ -10,6 +10,63 @@ var photo;
 
 var db = new mongodb.Db('bliss', new mongodb.Server('127.0.0.1', 27017), {safe:true});
 
+
+router.put('/photo/:category/:postid',function(req,res,next) {
+      var hash = new Date();
+    var activeSession = req.cookies._a;
+    var category = req.params.category;
+    var postId = req.params.postid;
+
+    photo = req.body.photo;
+          
+            
+          console.log(photo);
+
+     if (activeSession == md5(hash.getDay()+'87155')) {
+        if (category && postId && photo) {
+            console.log('posted');
+        
+               
+                //save to db
+                db.open(function(err) {
+                    if (!err) {
+                        db.collection(category,function(err,collection) {
+                            if (photo != "" || photo != null) {
+                                console.log('aa - >' + photo);
+
+                                collection.update({_id:ObjectId(postId)},{$push: {"photos": {"items":photo}}},function(err,result) {
+                                    if (err) {
+                                        console.log(err);
+                                    }
+                                    console.log('photo added.');
+                                     res.send({'status':1});  
+                                    db.close();
+                                });
+                            } else {
+                                console.log('empty field, record not created');
+                            }
+                            photo = "";
+                            res.send({'status':0});  
+
+                        });
+                    } else {
+                        res.render('admin',{msg: err});
+                    }
+                });
+        }
+     } else {
+        if (category) {
+                console.log('no access');
+                var err = new Error('Not Found');
+                err.status = 404;
+                next(err);
+        } else {
+            res.render('admin',"");
+        }
+    }
+});
+
+
 router.delete('/photo/:category/:postid',function(req,res,next) {
       var hash = new Date();
     var activeSession = req.cookies._a;
