b641a4fdb298bc1c20c37aa10ea8db015c91990c
aramik <aramik@whalerockindustries.com>
2014-08-06 23:15:16 -0700

    added delete rout

--- app.js
+++ app.js
@@ -50,18 +50,28 @@ app.set('view engine', 'html');
 
 
 //Routes
+app.use('/api/delete',deleteRoutes);
 app.use('/dashboard/view',getRoutes);
 app.use('/dashboard', admin);
 app.use('/', routes);
 
-app.delete('/api/photo/:category/:photo',function(req,res) {
-    res.write('hello');
-
-});
 
 
 
-
+var allowCrossDomain = function(req, res, next) 
+{
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  if( req.method.toLowerCase() === "options" )
+      {
+        res.send( 200 );
+      }
+  else
+      {
+    next();
+      }
+}
 
 
 
