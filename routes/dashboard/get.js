f177d9002ce61d8152ce58f6168d0985a2bcd30e
aramik <aramik@whalerockindustries.com>
2014-08-06 23:24:17 -0700

    added delete rout

--- routes/dashboard/get.js
+++ routes/dashboard/get.js
@@ -10,7 +10,53 @@ var router = express.Router();
 var db = new mongodb.Db('bliss', new mongodb.Server('127.0.0.1', 27017), {safe:true});
 
 router.delete('/:category',function(req,res,next) {
-    console.log('delete');
+      var hash = new Date();
+    var activeSession = req.cookies._a;
+    var category = req.params.category;
+    var postId = req.params.postid;
+
+            var photo = req.body.photo;
+          
+            console.log("id: " + updateId + " , photo: " + photo);
+          return;
+            
+          
+
+     if (activeSession == md5(hash.getDay()+'87155')) {
+        if (category) {
+            console.log('posted');
+        
+               
+                //save to db
+                db.open(function(err) {
+                    if (!err) {
+                        db.collection(category,function(err,collection) {
+                            if (photo != "" || photo != null) {
+                                collection.update({_id:OnjectId(postId)},{$pull: {"photos": {"items":photo}}});
+                                    console.log('photo removed. - >' + photo);
+                                    db.close();
+                            } else {
+                                console.log('empty field, record not created');
+                            }
+                            photo = "";
+                            res.render('admin',{msg: "photo removed"});  
+
+                        });
+                    } else {
+                        res.render('admin',{msg: err});
+                    }
+                });
+        }
+     } else {
+        if (category) {
+                console.log('no access');
+                var err = new Error('Not Found');
+                err.status = 404;
+                next(err);
+        } else {
+            res.render('admin',"");
+        }
+    }
 });
 
 
