db128202b7fa1cb172795c87bf547ef0929be08f
aramik <aramik@whalerockindustries.com>
2014-08-08 16:10:01 -0700

    updated tools

--- app.js
+++ app.js
@@ -40,6 +40,7 @@ var admin = require('./routes/dashboard/admin');
 var tools = require('./routes/dashboard/tools');
 var getRoutes = require('./routes/dashboard/get');
 var deleteRoutes = require('./routes/dashboard/delete');
+var application = require('./routes/application');
 
 
 
@@ -52,6 +53,7 @@ app.set('view engine', 'html');
 //Routes
 app.use('/dashboard/view',getRoutes);
 app.use('/dashboard', admin);
+app.use('/application', application);
 app.use('/', routes);
 
 
