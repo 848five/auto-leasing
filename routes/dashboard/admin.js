765feb97ada5b0ce6271d95d803bb361ccf85c6c
aramik <aramik@whalerockindustries.com>
2014-08-11 15:32:52 -0700

    updated

--- routes/dashboard/admin.js
+++ routes/dashboard/admin.js
@@ -36,7 +36,7 @@ router.post('/', function(req,res) {
 	user = req.body.user;
 	var pass = req.body.password;
 	var error = "";
-	var auth;
+	var _isAuthorized = "";
 
 	if (user == "" || pass == "") {
 		error = "enter a username and password.";
@@ -52,15 +52,15 @@ router.post('/', function(req,res) {
 							} else {
 								var hash = new Date();
 		                		res.cookie('_a', md5(hash.getDay()+'87155'), { expires: 0, httpOnly: true });
-		                		auth = 1;
+		                		_isAuthorized = 1;
 							}
 
 					db.close();
 				});
 			});
 		});
-		console.log(auth);
-		if (auth) {
+		console.log(_isAuthorized);
+		if (_isAuthorized) {
 			console.log('admin');
 			db.open(function(err) {
 			db.collection('applications',function(err,collection) {
