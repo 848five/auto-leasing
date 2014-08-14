24fff16b865924cd671a1e119a6a125160b42940
aramik <aramik@whalerockindustries.com>
2014-08-13 22:18:47 -0700

    upd8

--- routes/index.js
+++ routes/index.js
@@ -114,7 +114,8 @@ router.get('/:category?/:year?/:make?/:model?', function(req,res,next) {
                 request('https://api.edmunds.com/api/vehicle/v2/makes?state=new&fmt=json&api_key=vve9rc8s95q77kat7cc9h54m', function (error, response, body) {
                   if (!error && response.statusCode == 200) {
                     console.log(JSON.parse(body)); // Print the body of response.
-                    res.render('index',{makes:JSON.parse(body.responseJSON)});
+
+                    res.render('index',{makes:JSON.parse(body).responseJSON});
                   }
                 });
 
