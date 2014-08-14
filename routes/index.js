beb8954f45a74c68cddafbfcc909f8e99136703e
aramik <aramik@whalerockindustries.com>
2014-08-13 23:30:51 -0700

    upd8

--- routes/index.js
+++ routes/index.js
@@ -117,7 +117,13 @@ router.get('/:category?/:year?/:make?/:model?', function(req,res,next) {
                     url: url,
                     json: true
                 }, function (error, response, body) {
-                    console.log(body.type)
+                    var data = JSON.parse(body);
+                    console.log(data);
+                    console.log('-----');
+                    console.log(data.responseJSON);
+                    console.log(data.makes);
+                    console.log(data.name);
+
                     if (!error && response.statusCode === 200) {
                          res.render('index',{makes:body});
                     }
